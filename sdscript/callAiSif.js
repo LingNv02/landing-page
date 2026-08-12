02e55296a65769f0586de2d5d2018c82f3ac4e48
John Doe <155935094+LingNv02@users.noreply.github.com>
2026-08-12 16:12:22 +0700

    fix call ai options

--- sdscript/callAiSif.js
+++ sdscript/callAiSif.js
@@ -3,7 +3,9 @@ async function _sd_main(arg) {
     viewName: arg?.viewName,
     query: arg?.query,
     userPrompt: arg?.userPrompt,
+    hit: 8,
   };
+
   const content = await call_ai(arg?.userPrompt, options);
   return content;
 }
